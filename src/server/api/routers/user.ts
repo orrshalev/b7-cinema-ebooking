643b3f19fe6fc8f2720071c1c6cea9231712d80a
jennyngo1925 <jennyngo1925@gmail.com>
2023-03-16 13:53:07 -0400

    updated prisma

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -2,4 +2,13 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const userRouter = createTRPCRouter({})
+export const userRouter = createTRPCRouter({
+  getMovies: publicProcedure
+    .input(
+      z.object({ limit: z.number(), comingSoon: z.boolean(), date: z.date() })
+    )
+    .query(async ({ input, ctx }) => {
+      const moviesList = await ctx.prisma.user.findMany({ take: 1 });
+      return moviesList;
+    }),
+});
