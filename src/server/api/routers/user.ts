30b5d8c499f08421d7c573d5245b704eccff0467
David Gonzalez <djgonzalez0209@gmail.com>
2023-03-16 14:21:13 -0400

    user.ts

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -2,4 +2,15 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const userRouter = createTRPCRouter({})
+export const userRouter = createTRPCRouter({
+    createUser: publicProcedure
+    .input(
+        z.object({ email: z.string(), firstName: z.string(), lastName: z.string(), phoneNumber: z.string(),
+        password: z.string(), homeAddress: z.string(), cardNumber: z.string(), billAddress: z.string(),
+        cvv: z.string(), state: z.string()})
+        )
+        .query(async({input, ctx}) => { 
+            ctx.prisma.
+
+        }
+})
