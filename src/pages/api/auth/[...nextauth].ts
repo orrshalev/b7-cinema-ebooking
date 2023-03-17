adc4804215273bf234bd846ca0d72beca40980b1
jennyngo1925 <jennyngo1925@gmail.com>
2023-03-16 21:11:14 -0400

    moved database query to next-auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,62 +1,40 @@
-// /* eslint-disable @typescript-eslint/no-unsafe-argument */
-// /* eslint-disable @typescript-eslint/no-unsafe-call */
-// /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-// /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-// import NextAuth from "next-auth";
-// // import { authOptions } from "~/server/auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import { compare } from 'bcryptjs';
-// import { PrismaClient } from "@prisma/client";
-// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth, {NextAuthOptions} from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { prisma } from '~/server/db';
+import { api } from '~/utils/api';
 
-// const prisma = new PrismaClient();
-// const User = {
-//     email: "jenny@gmail.com",
-//     password: "abc123"
-// }
+const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt'
+  },
+  providers:[
+    CredentialsProvider({
+      type: 'credentials',
+      credentials: {
+        email: { label: "email", type: "email", placeholder: "me@email.com"},
+        password: { label: "Password", type: "password"}
+      },
+      async authorize(credentials, req){
+        const {email, password} = credentials as {
+          email: string;
+          password: string;
+        };
 
-// export default NextAuth({
-//     adapter: PrismaAdapter(prisma),
-//     providers:[
-//         CredentialsProvider({
-//             id: "credentials",
-//             name:"credentials",
-//             credentials: {
-//                 email: {
-//                     label: "Email",
-//                     type: "text",
-//                 },
-//                 password: {
-//                     label: "Password",
-//                     type: "password",
-//                 }
-//             },
-//             async authorize(credentials){
-//                 const user = await prisma.user.findFirst({where: {email:credentials?.email}});
-//                 console.log(credentials?.email);
-//                 console.log(credentials?.password);
-//                 if(!user) {
-//                     console.log("No user found with this email. Please sign up!")
-//                 }
-                
-//                 const checkPassword = await compare(credentials!.password, user.password);
+        const user = await prisma.user.findFirst({where: {email: email}})
 
-//                 if (!checkPassword || user?.password !== credentials?.email) {
-//                     console.log("Email or Password does not match");
-//                 }   
-//                 // const user = User;
-//                 // console.log(credentials);
-//                 // const checkPassword = await compare(credentials!.password, user.password);
-//                 // if (!checkPassword) console.log("error");
-
-//              return user;
-        
-//             }
-//         })
-//     ]
-// })
-
-import NextAuth from "next-auth";
-import { authOptions } from "~/server/auth";
+        // throw new Error(user?.email)
+        if (user == null) {
+          throw new Error('Invalid email or password')
+        }
+        if (user.email == email) {
+          throw new Error('Login Successful!')
+        }
+      }
+    })
+  ],
+  pages: {
+    signIn: '../../login'
+  }
+}
 
 export default NextAuth(authOptions);
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
-  getMovies: publicProcedure
+  getUser: publicProcedure
     .input(
       z.object({ email: z.string() })
     )
     .query(async ({ input, ctx }) => {
-      const findUser = await ctx.prisma.user.findMany({ take: 1 });
+      const findUser = await ctx.prisma.user.findMany({take: 1})
       return findUser;
     }),
 });
