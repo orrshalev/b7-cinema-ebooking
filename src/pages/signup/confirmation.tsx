b8368807bb34acb9bc984c2d28c68468bc787327
Orr Shalev <shalev.orr@gmail.com>
2023-03-17 20:44:14 -0400

    Confirmation working

--- src/pages/signup/confirmation.tsx
+++ src/pages/signup/confirmation.tsx
@@ -11,6 +11,8 @@ import Link from "next/link";
 
 const Confirmation: NextPage = () => {
   const router = useRouter();
+  const { email } = router.query;
+  const confirmationMutation = api.user.confirmUser.useMutation();
 
   type SubmitValues = {
     code: string;
@@ -19,13 +21,21 @@ const Confirmation: NextPage = () => {
     values: SubmitValues,
     { setSubmitting }: FormikHelpers<SubmitValues>
   ) => {
-    const result = await api.user.confirmUser(values.code);
-    setSubmitting(false);
+    if (typeof email === "string") {
+      const confirmed = await confirmationMutation.mutateAsync({
+        email: email,
+        confirmCode: values.code,
+      });
 
-    if (signupMutation.error) {
-      alert(signupMutation.error.message);
-    } else {
-      await router.push("/signup/confirmation?email=" + values.email);
+      setSubmitting(false);
+
+      if (confirmationMutation.error) {
+        alert(confirmationMutation.error.message);
+      } else if (!confirmed) {
+        alert("Something went wrong! Try again.");
+      } else {
+        await router.push("/login");
+      }
     }
   };
 
@@ -38,26 +48,29 @@ const Confirmation: NextPage = () => {
       </Head>
       <Navbar />
       <main className="flex min-h-screen flex-col items-center ">
-        <Formik initialValues={{ code: "" }} onSubmit={}>
-          <div className="my-auto grid w-full place-content-center rounded-md bg-white p-6 shadow-md lg:max-w-xl">
+        <Formik initialValues={{ code: "" }} onSubmit={handleSignup}>
+          <Form className="my-auto grid w-full place-content-center rounded-md bg-white p-6 shadow-md lg:max-w-xl">
             <h1 className="py-6 text-center text-3xl font-semibold text-dark-red">
               Registration Successful!
             </h1>
             <h4 className="pb-5 text-center text-dark-red">
               Please check your email for confirmation code
             </h4>
-            <input
+            <Field
               className=" border- mb-10 w-full rounded-md border-2 border-solid bg-white px-4 py-2 tracking-wide text-dark-red transition-colors  duration-200 focus:bg-white"
               type="text"
+              name="code"
               placeholder="Enter Confirmation Code"
             />
-            <Link
-              href="/login"
+            {/* Should be Link maybe */}
+            <button
+              type="submit"
+              // href="/login"
               className="focus:outline-solid w-full transform rounded-md bg-dark-red px-4 py-2 text-center tracking-wide text-white transition-colors duration-200 hover:bg-light-coral focus:bg-light-coral focus:outline-light-coral"
             >
               Login
-            </Link>
-          </div>
+            </button>
+          </Form>
         </Formik>
       </main>
       <Footer />
