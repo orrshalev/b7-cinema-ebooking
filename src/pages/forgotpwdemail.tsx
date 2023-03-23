b57f9b85132318cfaae3c146150c09ab47a2cedd
jennyngo1925 <jennyngo1925@gmail.com>
2023-03-22 21:50:52 -0400

    click handler to submit handler

--- src/pages/forgotpwdemail.tsx
+++ src/pages/forgotpwdemail.tsx
@@ -6,6 +6,7 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import { user } from "@userfront/core";
 import router, { useRouter } from "next/router";
+import { FormikHelpers, Formik, Form, Field } from "formik";
 
 interface mailConfigurations {
   to: string;
@@ -14,19 +15,26 @@ interface mailConfigurations {
 }
 
 const Pwdemail: NextPage = () => {
-  const [userEmail, setUserEmail] = useState({ email: "" });
   const router = useRouter();
 
-  const onSubmit = async () => {
+  type SubmitValues = {
+    email: string;
+  };
+  const onSubmit = async (
+    values: SubmitValues,
+    { setSubmitting }: FormikHelpers<SubmitValues>
+  ) => {
       const response = await fetch('../api/sendConfirmationEmail', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(userEmail.email),
+        body: JSON.stringify(values.email),
       });
+
+      setSubmitting(false);
     if (response.status == 200) {
-      await router.push("/forgotpwdverification?email=" + userEmail.email);
+      await router.push("/forgotpwdverification?email=" + values.email);
     } else {
       alert("Email is not registered. Please sign up for an account!")
     }
@@ -41,30 +49,28 @@ const Pwdemail: NextPage = () => {
       </Head>
       <Navbar />
       <main className="flex min-h-screen flex-col items-center ">
-        <div className="my-auto grid w-full place-content-center rounded-md bg-white p-6 shadow-md lg:max-w-xl">
+      <Formik initialValues={{ email: "" }} onSubmit={onSubmit}>
+          <Form className="my-auto grid w-full place-content-center rounded-md bg-white p-6 shadow-md lg:max-w-xl">
           <h1 className="py-6 text-center text-3xl font-semibold text-dark-red">
             Forgot Password
           </h1>
           <h4 className="pb-5 text-center text-dark-red">
             Please enter your email address associated with your account
           </h4>
-          <input
-            value={userEmail.email}
-            onChange={({ target }) =>
-              setUserEmail({ ...userEmail, email: target.value })
-            }
+          <Field
             type="email"
+            name="email"
             placeholder="Enter Email Address"
             className=" mb-10 w-full rounded-md border-solid bg-white px-4 py-2 tracking-wide text-dark-red transition-colors  duration-200 focus:bg-white"
-            required
-          />
+            />
           <button
-            onClick={onSubmit}
+          type="submit"
             className="focus:outline-solid w-full transform rounded-md bg-dark-red px-4 py-2 text-center tracking-wide text-white transition-colors duration-200 hover:bg-light-coral focus:bg-light-coral focus:outline-light-coral"
             >
             Send Verification Code
           </button>
-        </div>
+          </Form>
+        </Formik>
       </main>
       <Footer />
     </>
